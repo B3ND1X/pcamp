1b982a6dedff24404f68e428f23373aed351fb05
Liam Bendix <48177481+B3ND1X@users.noreply.github.com>
2021-11-16 21:14:26 -0500

    update

--- main.js
+++ main.js
@@ -19,3 +19,26 @@ shareButton.addEventListener('click', event => {
 closeButton.addEventListener('click', event => {
   shareDialog.classList.remove('is-open');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+try{Typekit.load();}catch(e){
+}
+
+$(function() {
+  $('#popup').animate({'bottom': '-14%'}, 1000).animate({'bottom': '-4%'}, 75).animate({'bottom': '-14%'}, 75);
+  $('.popup-close-icon').on('click', function(){
+  	$('#popup').animate({'bottom': '1000px'}, 500, function() {
+    	$(this).remove();
+    });
+  });
+});
